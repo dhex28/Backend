618bdb302032f5d39d4d3317333d8e80750daefc
rustom <145270393+arkieno@users.noreply.github.com>
2023-11-30 05:29:47 -0800

    b

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -13,7 +13,7 @@ class ProductModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['product_name', 'category_id', 'price', 'quantity', 'status', 'product_image'];
+    protected $allowedFields    = ['product_name', 'category_id', 'price', 'quantity', 'status','product_image'];
 
     // Dates
     protected $useTimestamps = false;
