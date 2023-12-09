b646c52c3fe89663f7d78e129400721b9d6d23a7
dhex28 <dextherbdelmonte@gmail.com>
2023-12-09 14:11:40 +0800

    bbbb

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,7 +13,23 @@ $routes->match(['post', 'get'], '/getData', 'MainController::getData');
 $routes->post('/save', 'MainController::save');
 $routes->match(['post', 'get'], '/amenitiesgetData', 'MainController::amenitiesgetData');
 $routes->post('/amenitiesSave', 'MainController::amenitiesSave');
-$routes->match(['post', 'get'], '/getCategory', 'MainController::getCategory');
-$routes->post('/saveCateg', 'MainController::saveCateg');
-$routes->match(['post', 'get'], '/getInventory', 'MainController::getInventory');
-$routes->post('/inventorySave', 'MainController::inventorySave');
+
+
+$routes->match(['post', 'get'], '/getCategory', 'ProductController::getCategory');
+$routes->post('/saveCateg', 'ProductController::saveCateg');
+$routes->match(['post', 'get'], '/getInventory', 'ProductController::getInventory');
+$routes->post('/inventorySave', 'ProductController::inventorySave');
+
+
+
+//pos inventory
+
+$routes->get('/', 'Home::index');
+$routes->get('/api/sales/(:any)', 'Home::getSales/$1');
+$routes->match(['get', 'post'], '/api/isales', 'Home::isales');
+$routes->match(['get', 'post'], '/api/setsales/(:any)', 'Home::setsales/$1');
+$routes->match(['get', 'post'], '/api/getProducts', 'Home::getProducts');
+$routes->match(['get', 'post'], '/api/updateQuantity', 'Home::updateQuantity');
+$routes->match(['get', 'post'], '/api/audit/(:any)', 'Home::audit/$1');
+$routes->match(['get', 'post'], '/api/newproduct', 'Home::newproduct');
+$routes->put('api/updateproduct/(:num)', 'Home::updateProduct/$1');
